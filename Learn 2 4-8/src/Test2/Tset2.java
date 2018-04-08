e8d4906786a9a220344741ce2700231546041044
1140068696@qq.com <1140068696@qq.com>
2018-04-08 22:27:52 +0800

    rainbow

---
+++ Learn 2 4-8/src/Test2/Tset2.java
@@ -0,0 +1,19 @@
+/**
+ * zzl
+ * version 2018 4-8
+ * 21:14Pm
+ *
+ *
+ * */
+
+
+package Test2;
+
+public class Tset2 {
+    public static final double CM_PRE_INCH = 2.54;
+    public static void main(String[] args){
+        double paperWidth= 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters:"+paperWidth*CM_PRE_INCH+" by "+paperHeight*CM_PRE_INCH);
+    }
+}
